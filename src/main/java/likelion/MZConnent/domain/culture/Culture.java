0d493a6efcc2417790d8d5070fea915084db3ba9
oosedus <seayeon0729@gmail.com>
2024-07-29 21:56:30 +0900

    fix: 모임 생성시 문화에 clubCount 업데이트 코드 추가

--- src/main/java/likelion/MZConnent/domain/culture/Culture.java
+++ src/main/java/likelion/MZConnent/domain/culture/Culture.java
@@ -8,11 +8,13 @@ import likelion.MZConnent.domain.review.Review;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class Culture {
     @Id
