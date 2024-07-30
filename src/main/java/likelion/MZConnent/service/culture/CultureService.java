491af092f792ce22ba68802395a2f8b3b24b054e
chaeyoungeee <chaeinge@gmail.com>
2024-07-30 22:45:50 +0900

    feat: 관심 문화 토글
    관심 문화가 토글되는 기능을 추가했음
    이미 있을 시 -> 삭제, 없을 시 -> 추가하는 로직으로 개발

--- src/main/java/likelion/MZConnent/service/culture/CultureService.java
+++ src/main/java/likelion/MZConnent/service/culture/CultureService.java
@@ -1,12 +1,16 @@
 package likelion.MZConnent.service.culture;
 
 import likelion.MZConnent.domain.culture.Culture;
+import likelion.MZConnent.domain.culture.CultureInterest;
+import likelion.MZConnent.domain.member.Member;
 import likelion.MZConnent.domain.review.Review;
 import likelion.MZConnent.dto.culture.response.CultureDetailResponse;
 import likelion.MZConnent.dto.culture.response.CulturesSimpleResponse;
 import likelion.MZConnent.dto.paging.response.PageContentResponse;
 import likelion.MZConnent.dto.review.response.ReviewsSimpleResponse;
+import likelion.MZConnent.repository.culture.CultureInterestRepository;
 import likelion.MZConnent.repository.culture.CultureRepository;
+import likelion.MZConnent.repository.member.MemberRepository;
 import likelion.MZConnent.repository.review.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,6 +30,9 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class CultureService {
     private final CultureRepository cultureRepository;
+    private final CultureInterestRepository cultureInterestRepository;
+    private final MemberRepository memberRepository;
+
     private final int PAGE_SIZE = 6;
 
     public PageContentResponse<CulturesSimpleResponse> getCulturesSimpleList(Long cultureCategoryId, int page) {
@@ -65,11 +72,55 @@ public class CultureService {
     }
 
     public CultureDetailResponse getCultureDetailInfo(Long cultureId) {
-        Culture culture = cultureRepository.findById(cultureId).orElseThrow(() -> {
-            log.info("해당 문화가 존재하지 않습니다.");
-            return new IllegalArgumentException("해당 문화가 존재하지 않습니다.");
-        });
+        Culture culture = getCultureById(cultureId);
         return CultureDetailResponse.builder()
                 .culture(culture).build();
+    }
+
+
+    // 문화 관심 토글
+    public boolean toggleCultureInterest(String email, Long cultureId) {
+
+        Member member = memberRepository.findByEmail(email).orElseThrow(() -> {
+            log.info("해당 회원이 존재하지 않음.");
+            return new IllegalArgumentException("해당 회원이 존재하지 않습니다.");
+        });
+        Culture culture = getCultureById(cultureId);
+
+        boolean exists = cultureInterestRepository.existsByMemberAndCulture(member, culture);
+
+
+        if (exists) { // 이미 문화를 관심 등록 해놓은 경우 (삭제)
+            CultureInterest cultureInterest = cultureInterestRepository.findByMemberAndCulture(member, culture)
+                    .get();
+            cultureInterestRepository.delete(cultureInterest);
+
+            member.getCultureInterests().remove(cultureInterest);
+            culture.getCultureInterests().remove(cultureInterest);
+            culture.setInterestCount(culture.getInterestCount() - 1);
+            return false;
+        }
+        else { // 문화를 관심 등록 해놓지 않은 경우 (추가)
+            CultureInterest cultureInterest = cultureInterestRepository.save(CultureInterest.builder()
+                    .member(member)
+                    .culture(culture)
+                    .build());
+
+            member.getCultureInterests().add(cultureInterest);
+            culture.getCultureInterests().add(cultureInterest);
+            culture.setInterestCount(culture.getInterestCount()+1);
+
+            return true;
+        }
+
+
+    }
+
+    private Culture getCultureById(Long cultureId) {
+        Culture culture = cultureRepository.findById(cultureId).orElseThrow(() -> {
+            log.info("해당 문화가 존재하지 않음.");
+            return new IllegalArgumentException("해당 문화가 존재하지 않습니다.");
+        });
+        return culture;
     }
 }
