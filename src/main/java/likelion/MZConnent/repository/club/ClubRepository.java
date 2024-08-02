e48454e517368c5a48971353eeece2936a159a03
oosedus <seayeon0729@gmail.com>
2024-08-03 03:20:18 +0900

    refactor: 필요없는 주석 제거

--- src/main/java/likelion/MZConnent/repository/club/ClubRepository.java
+++ src/main/java/likelion/MZConnent/repository/club/ClubRepository.java
@@ -14,16 +14,6 @@ import java.util.Collection;
 public interface ClubRepository extends JpaRepository<Club, Long> {
 
     // status가 OPEN인 클럽만 조회
-    // cultureId와 regionId가 0이면 무시
-    // cultureId와 regionId가 0이 아니면 해당 cultureId와 regionId에 맞는 클럽만 조회
-//    @Query("SELECT c FROM Club c WHERE c.status = 'OPEN' " +
-//            "AND (:cultureId = 0 OR c.culture.cultureId = :cultureId) " +
-//            "AND (:regionId = 0 OR c.region.regionId = :regionId)")
-//    Page<Club> findAllByFilters(@Param("cultureId") Long cultureId,
-//                                @Param("regionId") Long regionId,
-//                                Pageable pageable);
-
-    // status가 OPEN인 클럽만 조회
     // cultureCategoryId와 regionId가 0이면 무시
     // cultureCategoryId와 regionId가 0이 아니면 해당 cultureCategoryId와 regionId에 맞는 클럽만 조회
     @Query("SELECT c FROM Club c " +
