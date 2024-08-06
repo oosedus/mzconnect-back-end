800fe82d29958197a84bed32ef242b8539fadc9d
oosedus <seayeon0729@gmail.com>
2024-08-06 20:20:08 +0900

    fix: clubDetail DTO 수정

--- src/main/java/likelion/MZConnent/dto/club/LeaderDto.java
+++ src/main/java/likelion/MZConnent/dto/club/LeaderDto.java
@@ -1,5 +1,7 @@
 package likelion.MZConnent.dto.club;
 
+import likelion.MZConnent.domain.member.Age;
+import likelion.MZConnent.domain.member.Gender;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,12 +12,16 @@ import java.util.List;
 @NoArgsConstructor
 public class LeaderDto {
     private String username;
+    private Age age;
+    private Gender gender;
     private String profileImageUrl;
     private List<SelfIntroductionDto> selfIntroductions;
 
     @Builder
-    public LeaderDto(String username, String profileImageUrl, List<SelfIntroductionDto> selfIntroductions) {
+    public LeaderDto(String username,Age age,Gender gender, String profileImageUrl, List<SelfIntroductionDto> selfIntroductions) {
         this.username = username;
+        this.age=age;
+        this.gender=gender;
         this.profileImageUrl = profileImageUrl;
         this.selfIntroductions = selfIntroductions;
     }
